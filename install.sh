c2c5fb94443364d3c4d8323287d996bc85d6ea34
Japh <japhie@gmail.com>
2017-12-27 01:05:20 +1100

    Debugging installer

--- install.sh
+++ install.sh
@@ -195,7 +195,7 @@ gruve_do_install() {
     echo "=> Gruve already in crontab"
   fi
 
-  command /home/ethos/gruve-client/gruve-client --setup "${PROFILE_INSTALL_DIR}/"
+  command cd /home/ethos/gruve-client ; ./gruve-client --setup "${PROFILE_INSTALL_DIR}/"
 
   # Source Gruve
   # shellcheck source=/dev/null
